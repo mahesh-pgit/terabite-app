e0b1938e882881a9df20854ed215918bd0b703db
Mahesh <naravamaheshsaikumar.321@gmail.com>
2025-03-10 23:29:36 +0530

    ignited the app

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const app = React.createElement("h1", {}, "Hello from Tear Bite")
+ReactDOM.createRoot(document.getElementById("root")).render(app)
